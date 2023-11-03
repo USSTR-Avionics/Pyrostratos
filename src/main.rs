8dda7e7de2da9a58f8fdfab219af17ae42eae4fe
Adhyan H <adhyanpatel@gmail.com>
2023-11-03 05:46:15 -0600

    TODO: see if flashed code runs; print LED; semihosting; pin IO

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-                     // use panic_abort as _; // requires nightly
-                     // use panic_itm as _; // logs messages over ITM; requires ITM support
-                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
-use core::concat;
-use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
+use cortex_m_semihosting::{debug, hprintln};
+// ! needs to be imported for the linker to find interrupt vectors
+use stm32f3::stm32f303;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
     loop {
-        // your code goes here
-        asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-        hprintln!("Hello, world!");
+        hprintln!("Hello, World!");
     }
 }
+
