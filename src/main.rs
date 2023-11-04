14cad2b4cbaeefcf2fa4b4980b2814fbbd0e6531
Adhyan H <adhyanpatel@gmail.com>
2023-11-04 12:48:49 -0600

    0.0.2 - led blinks

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,45 @@
 
 extern crate panic_halt;
 
-use cortex_m::asm;
+use nb::block;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-use stm32f3xx_hal::{self as hal, pac, prelude::*};
-// use stm32f3::stm32f303;
+use stm32f1::stm32f103;
+use stm32f1xx_hal::{self as hal, pac, prelude::*, timer::Timer};
 
 
 #[entry]
 fn main() -> ! {
-
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
 
-    let mut rcc = dp.RCC.constrain();
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
 
-    // led is connected to LD3 on the dev board
-    // the pin is on PB_3
-    let mut led = gpiob.pb3.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
+    // Acquire the GPIOC peripheral
+    let mut gpioa = dp.GPIOA.split();
+
+    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // in order to configure the port. For pins 0-7, crl should be passed instead.
+    // let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut led = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
+    // Configure the syst timer to trigger an update every second
+    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
+    timer.start(1.Hz()).unwrap();
+
+    // Wait for the timer to trigger an update and change the state of the LED
     loop {
-        led.toggle().unwrap();
-        for i in 0..1_000 {
-            asm::nop();
-        }
+        block!(timer.wait()).unwrap();
+        led.toggle();
+        block!(timer.wait()).unwrap();
+        led.toggle();
     }
 }
 
