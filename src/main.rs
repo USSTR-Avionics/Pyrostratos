f9f5dbb7e3df78c710640d80ae945fc08e73d7ed
Adhyan H <adhyanpatel@gmail.com>
2023-11-04 02:54:53 -0600

    added hal; blinkly led no blink

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@
 
 extern crate panic_halt;
 
+use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
-// ! needs to be imported for the linker to find interrupt vectors
-use stm32f3::stm32f303;
+use cortex_m_semihosting::hprintln;
+use stm32f3xx_hal::{self as hal, pac, prelude::*};
+// use stm32f3::stm32f303;
+
 
 #[entry]
 fn main() -> ! {
+
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut rcc = dp.RCC.constrain();
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+
+    // led is connected to LD3 on the dev board
+    // the pin is on PB_3
+    let mut led = gpiob.pb3.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+
     loop {
-        hprintln!("Hello, World!");
+        led.toggle().unwrap();
+        for i in 0..1_000 {
+            asm::nop();
+        }
     }
 }
 
